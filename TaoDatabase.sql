b151a781980ffd501edf68012736a25d7ec208a7
Luong Quang Hung <111416811+kudo-2003@users.noreply.github.com>
2024-09-27 07:49:29 +0200

    Add files via upload

---
+++ TaoDatabase.sql
@@ -0,0 +1,14 @@
+﻿/*tạo database */
+create database EnglishFree;
+/*Sử chạy phần kế tiếp*/
+go 
+/*Sử dụng database đã tạo*/
+use EnglishFree;
+go
+/**/
+create table Users(
+	Id int primary key identity(1,1),
+	Email nvarchar(255) unique not null,
+	UserPassword nvarchar(255) not null 
+);
+
